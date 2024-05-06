b524472e5dd160699f7643b4308589786ec10de9
riltho1 <100237088+riltho1@users.noreply.github.com>
2024-05-06 12:26:44 +1200

    Still trying to organise enemy pathfinding

--- Jungle Jamboree/Scripts/Enemy.gd
+++ Jungle Jamboree/Scripts/Enemy.gd
@@ -1,22 +1,16 @@
 extends CharacterBody3D
 
-var player = null
-
 const SPEED = 4.0
-
-@export var player_path : NodePath
 
 @onready var nav_agent = $NavigationAgent3D
 
-func _ready():
-	player = get_node(player_path)
-
-func _process(delta: float) -> void:
-	var velocity: Vector3 = Vector3.ZERO
-
-	#Navigation
-	if player:
-		var new_nav_point: Vector3 = nav_agent.get_next_path_position()
-		velocity = (new_nav_point - global_transform.origin).normalized() * SPEED
-		
-		move_and_slide()
+func _physics_process(delta):
+	var current_location = global_transform.origin
+	var next_location = nav_agent.get_next_location()
+	var new_velocity = (next_location - current_location).normalised() * SPEED
+	
+	velocity = new_velocity
+	move_and_slide()
+	
+func update_target_location(target_location):
+	nav_agent.destination = target_location
