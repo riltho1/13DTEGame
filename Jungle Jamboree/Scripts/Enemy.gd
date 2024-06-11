f8c5bb720948423e468da9f18b1462e99c86d137
riltho1 <100237088+riltho1@users.noreply.github.com>
2024-06-11 13:26:28 +1200

    Trying to make enemy work still.

--- Jungle Jamboree/Scripts/Enemy.gd
+++ Jungle Jamboree/Scripts/Enemy.gd
@@ -3,14 +3,20 @@ extends CharacterBody3D
 @onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 var player_chase = false
 @export var speed = 200
-@export var player = get_tree().get_first_node_in_group("Player")
+@onready var player = get_tree().get_first_node_in_group("Player")
 
 func _ready():
 	nav_agent.set_velocity(Vector3.ZERO)
-	if player_chase:
-		velocity = (player.global_position - global_position).normalized() * speed
-		move_and_slide()
+
+func _physics_process(delta):
+	if player_chase and player:
+		var direction = (player.global_transform.origin - global_transform.origin).normalized()
+		nav_agent.set_velocity(direction * speed)
 
 func _on_area_3d_body_entered(body):
-		if player:
-			player_chase = true
+	if body.is_in_group("Player"):
+		player_chase = true
+
+func _on_area_3d_body_exited(body):
+	if body.is_in_group("Player"):
+		player_chase = false
