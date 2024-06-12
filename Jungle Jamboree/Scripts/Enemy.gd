473caddadf2d7f099421538da11e2a5e35130958
riltho1 <100237088+riltho1@users.noreply.github.com>
2024-06-12 15:00:08 +1200

    Got the the enemy code working so that it will chase the player in the collision box.

--- Jungle Jamboree/Scripts/Enemy.gd
+++ Jungle Jamboree/Scripts/Enemy.gd
@@ -2,21 +2,29 @@ extends CharacterBody3D
 
 @onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 var player_chase = false
-@export var speed = 200
-@onready var player = get_tree().get_first_node_in_group("Player")
+@export var speed = 8
+@onready var player: Node3D = get_tree().get_first_node_in_group("Player")
+@onready var vision_area: Area3D = $Area3D
 
 func _ready():
 	nav_agent.set_velocity(Vector3.ZERO)
 
 func _physics_process(delta):
+	var overlapping_bodies = vision_area.get_overlapping_bodies()
+	# Check if the player is inside the vision area
+	if player in overlapping_bodies:
+		player_chase = true
+	else:
+		player_chase = false
 	if player_chase and player:
 		var direction = (player.global_transform.origin - global_transform.origin).normalized()
 		nav_agent.set_velocity(direction * speed)
+	else:
+		nav_agent.set_velocity(Vector3.ZERO)
+	
+	velocity = nav_agent.velocity
+	move_and_slide()
 
-func _on_area_3d_body_entered(body):
-	if body.is_in_group("Player"):
-		player_chase = true
-
-func _on_area_3d_body_exited(body):
-	if body.is_in_group("Player"):
-		player_chase = false
+	
+	
+	
