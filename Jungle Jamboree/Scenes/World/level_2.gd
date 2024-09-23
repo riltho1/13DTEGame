a5c1b8320920174ba2f98df9bb8760e6a51bfb8e
riltho1 <100237088+riltho1@users.noreply.github.com>
2024-09-24 10:56:06 +1200

    level transfer between scenes

--- Jungle Jamboree/Scenes/World/level_2.gd
+++ Jungle Jamboree/Scenes/World/level_2.gd
@@ -1,16 +1,15 @@
-extends Area3D
+extends Node3D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta: float) -> void:
 	pass
 
-
-func _on_body_entered(body):
+func _on_shadow_area_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://Scenes/World/Level2.tscn")
+		get_tree().change_scene_to_file("res://Scenes/World/Level3.tscn")
